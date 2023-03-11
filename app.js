2d826f53dc6c8ffddc2e358b991dfc35aead2f67
bishal adhikari <bishalladhikari22@gmail.com>
2023-03-11 13:28:22 +0545

    change in js

--- app.js
+++ app.js
@@ -11,3 +11,10 @@ navLinks.forEach(link => {
     document.body.classList.remove('nav-open');
   });
 });
+const aboutLink = document.querySelector('.about-link');
+const aboutSection = document.getElementById('about');
+
+aboutLink.addEventListener('click', (event) => {
+  event.preventDefault();
+  aboutSection.scrollIntoView({ behavior: "smooth" });
+});
